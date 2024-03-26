c710295533e2775066827db2ced0a1ca1acfd69b
Jesse Sorsa <149471420+jessesorsa@users.noreply.github.com>
2024-03-26 15:01:10 +0200

    Update run.sh
    run.sh cleaned

--- run.sh
+++ run.sh
@@ -7,14 +7,7 @@ cleanup() {
 }
 trap cleanup EXIT TERM
 
-# echo "Waiting for 30 seconds for the services to start..."
-# sleep 30
-# echo "Starting the services..."
-
 (cd /home/pi/HTA && python3 receiver.py) & pids+=( "$!" )
 (cd /home/pi/HTA && python3 wserver.py) & pids+=( "$!" )
-
-# python receiver.py & pids+=( "$!" )
-# FLASK_APP=wserver.py FLASK_DEBUG=1 flask run --reload & pids+=( "$!" )
 
 wait
